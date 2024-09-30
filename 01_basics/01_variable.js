aec9a1d43b1d59cbfd41888ae03d0085e674ea76
MAC <mac@Apples-MacBook-Pro.local>
2024-09-30 23:46:33 +0530

    30 sep 2024

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,16 @@
+const accountID="123445";
+let accountEmail="mukesh.yadav@gmail.com";
+var accountPassword="23232";
+
+accountCity="Mumbai"
+
+let accountState;
+
+
+// accountId="2" holder
+accountID="kdbjknkcjn"
+accountEmail="rakesh@gmial.com";
+accountPassword="088990";
+accountCity="hydrabad"
+
+console.log(accountID,accountEmail,accountPassword,accountCity,accountState)
